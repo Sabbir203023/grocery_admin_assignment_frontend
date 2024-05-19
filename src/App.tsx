3a11fa475b18b73a0d2d627a85cbd5999bc9e723
[mdsabbir20] <[s.sabbirhossain188@gmail.com]>
2024-05-19 21:17:22 +0600

    app tsx fix error

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
 import NavBar from './components/navbar';
+import ProductTable from './components/producttable';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      
+      <ProductTable />
     </div>
   );
 }
